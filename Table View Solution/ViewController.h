ae9a3b50cb1866e843b2de1dac72f37a01bcb0ed
operationneenerneener <operationneenerneener@gmail.com>
2014-07-01 16:16:43 -0600

    Initial Commit

---
+++ Table View Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Table View Solution
+//
+//  Created by Dice's on 7/1/14.
+//  Copyright (c) 2014 InnovaCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Table View Solution/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Table View Solution
+//
+//  Created by Dice's on 7/1/14.
+//  Copyright (c) 2014 InnovaCo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
