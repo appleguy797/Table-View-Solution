12e2db7986cde1ad3e435d354fa2535870d34871
operationneenerneener <operationneenerneener@gmail.com>
2014-07-01 21:09:17 -0600

    updated commit

--- myTableViewController.h
+++ myTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  myTableViewController.h
 //  Table View Solution
 //
 //  Created by Dice's on 7/1/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface myTableViewController : UITableViewController
 
 @end
-
